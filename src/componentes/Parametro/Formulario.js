4e851faa3d63844a0becd0afb4ba1cde3ce65ea3
msueli <melisueli@gmail.com>
2022-08-14 16:49:47 -0400

    parametro
    actualizacion de formulario parametro

--- src/componentes/Parametro/Formulario.js
+++ src/componentes/Parametro/Formulario.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const Formulario = (props) => {
-    const [dato,setDato]= useState({tarea:""})
+    const [dato,setDato]= useState({mailSaliente:"",servidorMail:"",puertoMail:"",interesBcp:"",interesMoratorio:"",interesPunitorio:""})
     const handleChange = (e) => {
       setDato({
         ...dato,
@@ -16,30 +16,30 @@ const Formulario = (props) => {
   return (
     <>
         <div className="container">
-            <div className="row">
+            <div className="row ">
                 <div class="mb-3">
-                    <label for="mail-saliente" class="form-label">Email sistema</label>
-                    <input name="mail_saliente" type="email" class="form-control" id="mail-saliente" placeholder="name@example.com"/>
+                    <label for="mailSaliente" class="form-label">Email sistema</label>
+                    <input name="mailSaliente" type="email" class="form-control" id="mailSaliente" placeholder="name@example.com" onChange={handleChange} value={dato.mailSaliente}/>
                 </div>
                 <div class="mb-3">
-                    <label for="servidor-mail" class="form-label">Servidor email</label>
-                    <input name="servidor-mail" type="text" class="form-control" id="servidor-mail" placeholder="www.example.com"/>
+                    <label for="servidorMail" class="form-label">Servidor email</label>
+                    <input name="servidorMail" type="text" class="form-control" id="servidorMail" placeholder="www.example.com" onChange={handleChange} value={dato.servidorMail}/>
                 </div>
                 <div class="mb-3">
-                    <label for="puerto-mail" class="form-label">Puerto email</label>
-                    <input name="puerto-mail" min="0" type="number" class="form-control" id="puerto-mail" />
+                    <label for="puertoMail" class="form-label">Puerto email</label>
+                    <input name="puertoMail" min="0" type="number" class="form-control" id="puertoMail"  onChange={handleChange} value={dato.puertoMail}/>
                 </div>
                 <div class="mb-3">
-                    <label for="interes-bcp" class="form-label">Interes BCP</label>
-                    <input name="interes-bcp" min="0" type="number" class="form-control" id="interes-bcp" />
+                    <label for="interesBcp" class="form-label">Interes BCP</label>
+                    <input name="interesBcp" min="0" type="number" class="form-control" id="interesBcp" onChange={handleChange} value={dato.interesBcp}/>
                 </div>
                 <div class="mb-3">
-                    <label for="interes-moratorio" class="form-label">Interes moratorio</label>
-                    <input name="interes-moratorio" min="0" type="number" class="form-control" id="interes-moratorio" />
+                    <label for="interesMoratorio" class="form-label">Interes moratorio</label>
+                    <input name="interesMoratorio" min="0" type="number" class="form-control" id="interesMoratorio" onChange={handleChange} value={dato.interesMoratorio}/>
                 </div>
                 <div class="mb-3">
-                    <label for="interes-punitorio" class="form-label">Interes punitorio</label>
-                    <input name="interes-punitorio" min="0" type="number" class="form-control" id="interes-punitorio" />
+                    <label for="interesPunitorio" class="form-label">Interes punitorio</label>
+                    <input name="interesPunitorio" min="0" type="number" class="form-control" id="interesPunitorio" onChange={handleChange} value={dato.interesPunitorio}/>
                 </div>
                 <div className="col-sm-2">
                     <button onClick={handleAdd} className="btn btn-success">Guardar</button>
