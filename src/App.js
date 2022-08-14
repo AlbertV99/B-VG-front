216eb6a26c4563c98f3e76a637c57320b188924b
msueli <melisueli@gmail.com>
2022-08-13 23:32:08 -0400

    agregar estilo boostrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-
-    
+import Panel from './componentes/Usuario/Panel'
 
 function App() {
   return (
     <div className="App">
-
+        <Panel/>
     </div>
   );
 }
