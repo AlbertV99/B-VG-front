74b8263efcac716c5fde253514c85f75c7c714f9
msueli <melisueli@gmail.com>
2022-08-15 15:41:25 -0400

    parametro
    modficacion del formulario y panel de parametros

--- src/componentes/Parametro/Formulario.js
+++ src/componentes/Parametro/Formulario.js
@@ -16,34 +16,44 @@ const Formulario = (props) => {
   return (
     <>
         <div className="container">
-            <div className="row ">
-                <div class="mb-3">
+            <div class="row">
+                <div class="col-lg-3">
                     <label for="mailSaliente" class="form-label">Email sistema</label>
                     <input name="mailSaliente" type="email" class="form-control" id="mailSaliente" placeholder="name@example.com" onChange={handleChange} value={dato.mailSaliente}/>
                 </div>
-                <div class="mb-3">
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
                     <label for="servidorMail" class="form-label">Servidor email</label>
                     <input name="servidorMail" type="text" class="form-control" id="servidorMail" placeholder="www.example.com" onChange={handleChange} value={dato.servidorMail}/>
                 </div>
-                <div class="mb-3">
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
                     <label for="puertoMail" class="form-label">Puerto email</label>
                     <input name="puertoMail" min="0" type="number" class="form-control" id="puertoMail"  onChange={handleChange} value={dato.puertoMail}/>
+                    </div>
                 </div>
-                <div class="mb-3">
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
                     <label for="interesBcp" class="form-label">Interes BCP</label>
                     <input name="interesBcp" min="0" type="number" class="form-control" id="interesBcp" onChange={handleChange} value={dato.interesBcp}/>
                 </div>
-                <div class="mb-3">
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
                     <label for="interesMoratorio" class="form-label">Interes moratorio</label>
                     <input name="interesMoratorio" min="0" type="number" class="form-control" id="interesMoratorio" onChange={handleChange} value={dato.interesMoratorio}/>
                 </div>
-                <div class="mb-3">
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
                     <label for="interesPunitorio" class="form-label">Interes punitorio</label>
                     <input name="interesPunitorio" min="0" type="number" class="form-control" id="interesPunitorio" onChange={handleChange} value={dato.interesPunitorio}/>
-                </div>
-                <div className="col-sm-2">
-                    <button onClick={handleAdd} className="btn btn-success">Guardar</button>
-                </div>
+            </div>
+            <div className="col-lg-3">
+                 <button onClick={handleAdd} className="btn btn-success">Guardar</button>
             </div>
         </div>
     </>
--- src/componentes/Parametro/Panel.js
+++ src/componentes/Parametro/Panel.js
@@ -1,5 +1,4 @@
 import React,{useState} from 'react'
-import Tabla from './Tabla'
 import Formulario from './Formulario'
 
 
@@ -17,35 +16,11 @@ const Panel = () => {
     return (
         <div className="container-fluid">
             <div className="row">
-                <h1 style={{color:"white"}}>Usuario</h1>
-            </div>
-            <div className="row">
-                <div className="col-sm-6">
-                    <form >
-                        <div className="row">
-                            <div className="col-sm-8">
-                                <input type="text" className="form-control" placeholder="Buscar..."/>
-                            </div>
-                            <div className="col-sm-4">
-                                <button className="btn-primary btn" type='submit'>Buscar</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div className="col-sm-6 text-end">
-                    <button className="btn btn-success" onClick={()=>{ setEstadoForm(!estadoForm)}}>{!estadoForm?"Nuevo":"Cerrar"}</button>
-                    <button className="btn btn-secondary" onClick={()=>{}}>Modificar</button>
-                    <button className="btn btn-danger" onClick={()=>{}}>Eliminar</button>
-                </div>
+                <h1 style={{color:"black"}}>Parametros</h1>
             </div>
             <div className="row" style={{paddingTop:"25px"}}>
-                {estadoForm && <Formulario/>}
+                <Formulario/>
                 <p></p>
-                <hr/>
-            </div>
-            <div className="row">
-                <br/>
-                <Tabla datos={datos}/>
             </div>
         </div>
     )
